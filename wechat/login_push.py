a89a2f320456d1960d9d6a8c4481f27774ba3d6f
Sun Song <sunsongxp@gmail.com>
2018-09-28 15:54:49 +0800

    Improve login_push.py

--- wechat/login_push.py
+++ wechat/login_push.py
@@ -1,19 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##################
+#
+# Simple Login and Push to Wechat Mini-program
+# Created by Song in Wiredcraft
+#
+##################
+#
+# ChangeLog
+# 20180928 Song Initial Creation
+#
+##################
+#
+# Usage:
+#   export SLACK_BOT_TOKEN=""
+#   export SLACK_CHANNELS=cicd
+#   python login_push.py version project_root description
+#
+##################
+
 import base64
 import os
 import re
 import requests
 import subprocess
+import sys
 
 
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN", "")
 SLACK_CHANNELS = os.environ.get("SLACK_CHANNELS", "")
+
+RETRY_LOGIN = os.environ.get("RETRY_LOGIN", "3")
+RETRY_PUSH = os.environ.get("RETRY_PUSH", "3")
+
+# Document: https://api.slack.com/methods/files.upload
+SLACK_UPLOAD_URL = "https://slack.com/api/files.upload"
 CMD_CLI = "/Applications/wechatwebdevtools.app/Contents/Resources/app.nw/bin/cli"
 
 
-def decode_image(base64_text, output_file):
+def decode_image(base64_text, output_file=None):
     img_data = base64.b64decode(re.sub('^data:image/.+;base64,', '', base64_text))
-    with open(output_file, "wb") as file:
-        file.write(img_data)
+    if output_file:
+        with open(output_file, "wb") as file:
+            file.write(img_data)
+    return img_data
 
 
 def execute(cmd):
@@ -27,14 +58,24 @@ def execute(cmd):
         raise subprocess.CalledProcessError(return_code, cmd)
 
 
-def login(retry):
+def login(retry=3):
     for line in execute([
         CMD_CLI,
         "-l",
         "--login-qr-output base64"]
     ):
         if line.startswith("data:image/jpeg;base64"):
-            decode_image(line, "qr.jpg")
+            image = decode_image(line)
+            result = requests.post(
+                SLACK_UPLOAD_URL,
+                files={'file': image},
+                data={
+                    "token": SLACK_BOT_TOKEN,
+                    "channels": SLACK_CHANNELS,
+                }
+            )
+            if result.status_code != 200:
+                print("Failed to upload the qrcode to slack!")
         elif line.startswith("login success"):
             print("Success")
             return True
@@ -65,7 +106,7 @@ def push(version, project_root, description, retry=3):
         # Looking for line:
         # error: '{"code":40000,"error":"错误 需要重新登录"}',
         if line.strip().startswith("error:") and '"code":40000' in line:
-            login(retry=3)
+            login(retry=int(RETRY_LOGIN))
             if retry > 0:
                 push(
                     version=version,
@@ -76,7 +117,14 @@ def push(version, project_root, description, retry=3):
 
 
 def main():
-    push("v0.0.1", "/Users/sloppysun/Desktop/test", "Description aaa", retry=3)
+    version, project_root, description = sys.argv[1:]
+
+    push(
+        version,
+        project_root,
+        description,
+        retry=int(RETRY_PUSH)
+    )
 
 
 if __name__ == "__main__":
